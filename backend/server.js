ed86c1f7a92e30825898b339f81894d481197b96
McK-Atk-222 <mckay.atkinson@gmail.com>
2025-04-29 17:17:23 -0600

    coded user and notes routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,9 @@ app.use(cookieParser())
 app.use('/', express.static(path.join(__dirname, '/public'))) //built in middleware
 
 app.use('/', require('./routes/root'))
+app.use('/auth', require('./routes/authRoutes'))
+app.use('/users', require('./routes/userRoutes.js'))
+app.use('/notes', require('./routes/noteRoutes.js'))
 
 app.all('*', (req, res) => {
     res.status(404)
