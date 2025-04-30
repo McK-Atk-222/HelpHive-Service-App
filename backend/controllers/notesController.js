6d08701eaf1c28d1f79e878b708a8e1f3a3b719c
McK-Atk-222 <mckay.atkinson@gmail.com>
2025-04-29 18:21:57 -0600

    coded auth controller and routes and middleware to verify jwt prior to access to data

--- backend/controllers/notesController.js
+++ backend/controllers/notesController.js
@@ -1,9 +1,8 @@
 const Note = require('../models/Note')
 const User = require('../models/User')
 
-// @desc Get all notes 
+
 // @route GET /notes
-// @access Private
 const getAllNotes = async (req, res) => {
     // Get all notes from MongoDB
     const notes = await Note.find().lean()
@@ -14,8 +13,6 @@ const getAllNotes = async (req, res) => {
     }
 
     // Add username to each note before sending the response 
-    // See Promise.all with map() here: https://youtu.be/4lqJBBEpjRE 
-    // You could also do this with a for...of loop
     const notesWithUser = await Promise.all(notes.map(async (note) => {
         const user = await User.findById(note.user).lean().exec()
         return { ...note, username: user.username }
@@ -24,9 +21,8 @@ const getAllNotes = async (req, res) => {
     res.json(notesWithUser)
 }
 
-// @desc Create new note
+
 // @route POST /notes
-// @access Private
 const createNewNote = async (req, res) => {
     const { user, title, text } = req.body
 
@@ -53,9 +49,8 @@ const createNewNote = async (req, res) => {
 
 }
 
-// @desc Update a note
+
 // @route PATCH /notes
-// @access Private
 const updateNote = async (req, res) => {
     const { id, user, title, text, completed } = req.body
 
@@ -89,9 +84,8 @@ const updateNote = async (req, res) => {
     res.json(`'${updatedNote.title}' updated`)
 }
 
-// @desc Delete a note
+
 // @route DELETE /notes
-// @access Private
 const deleteNote = async (req, res) => {
     const { id } = req.body
 
